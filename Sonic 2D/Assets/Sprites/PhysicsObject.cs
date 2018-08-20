33c2fcd49a6412c814c8d89caccc5478b641418a
Otterlord <yourotterlord@gmail.com>
2018-08-19 18:31:37 -0700

    Initial Commit

---
+++ Sonic 2D/Assets/Sprites/PhysicsObject.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsObject : MonoBehaviour {
+
+    protected Vector2 velocity;
+    protected Vector2 target;
+    protected Vector2 targetV;
+    protected Rigidbody2D rb;
+    public float grv = 9.8f;
+
+    protected bool grounded = false;
+    protected Vector2 groundNormal;
+
+    protected const float minMoveDist = 0.001f;
+    protected ContactFilter2D contactFilter;
+    protected RaycastHit2D[] hitBuffer = new RaycastHit2D[16];
+
+    public float minGroundNormal = 0.65f;
+
+    protected const float shellRadius = 0.01f;
+    protected List<RaycastHit2D> hitBufferList = new List<RaycastHit2D>(16); 
+
+	// Use this for initialization
+
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        contactFilter.useTriggers = false;
+        contactFilter.SetLayerMask(Physics2D.GetLayerCollisionMask(gameObject.layer));
+        contactFilter.useLayerMask = true;
+    }
+
+	// Update is called once per frame
+	void FixedUpdate () {
+        velocity = target;
+
+        grounded = false;
+
+        Vector2 deltaVelocity = velocity * Time.deltaTime;
+
+        Vector2 moveAlongGround = new Vector2(groundNormal.y, -groundNormal.x);
+
+        Vector2 move = moveAlongGround * deltaVelocity.x;
+        Move(move, false);
+
+        move = targetV * Time.deltaTime;
+
+        Move(move, true);
+	}
+
+    void Move (Vector2 movement, bool yMovement)
+    {
+        float distance = movement.magnitude;
+
+        if (distance > minMoveDist)
+        {
+            print("yey");
+            int count = rb.Cast(movement, contactFilter, hitBuffer, distance + shellRadius);
+            hitBufferList.Clear();
+            for (int i = 0; i < count; i++)
+            {
+                hitBufferList.Add(hitBuffer[i]);
+            }
+
+            for (int i = 0; i < hitBufferList.Count; i++)
+            {
+                Vector2 currentNormal = hitBufferList[i].normal;
+                //groundNormal = currentNormal;
+                grounded = true;
+                if (true) // currentNormal.y > minGroundNormal
+                {
+                    grounded = true;
+                    if (yMovement)
+                    {
+                        groundNormal = currentNormal;
+                        currentNormal.x = 0;
+                    }
+                }
+                float projection = Vector2.Dot(velocity, currentNormal);
+                if (projection < 0)
+                {
+                    velocity = velocity - projection * currentNormal;
+                }
+
+                float modifiedDistance = hitBufferList[i].distance - shellRadius;
+                distance = modifiedDistance < distance ? modifiedDistance : distance;
+            }
+        }
+        else print("!");
+        rb.position += movement.normalized * distance;
+    }
+}
---
+++ Sonic 2D/Assets/Sprites/PlayerBehaviour.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : PhysicsObject {
+
+    private float gsp = 0;
+    public float acc = 2;
+    public float dec = 20;
+    public float frc = 8;
+    public float topSpeed = 50;
+    public float rotSpd = 5;
+    private GameObject sprite;
+
+    void OnEnable ()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sprite = transform.GetChild(0).gameObject;
+    }
+
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            if (gsp > 0) gsp -= dec * Time.deltaTime;
+            else gsp -= acc * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            if (gsp < 0) gsp += dec * Time.deltaTime;
+            else gsp += acc * Time.deltaTime;
+        }
+        else
+        {
+            if (Mathf.Abs(gsp) < frc) gsp = 0;
+            else gsp -= frc * Mathf.Sign(gsp) * Time.deltaTime;
+        }
+
+        if (Mathf.Abs(gsp) > topSpeed) gsp = topSpeed * Mathf.Sign(gsp);
+
+        if (grounded) targetV = groundNormal * grv;
+        else
+        {
+            targetV += Vector2.up * grv * Time.deltaTime;
+            groundNormal = new Vector2(0, 1);
+        }
+            
+
+
+        target.x = gsp;
+        //if (Mathf.Abs(groundNormal.x) > .5f) transform.up = new Vector2(Mathf.Sign(groundNormal.x), 0);
+        //else if (Mathf.Abs(groundNormal.x) < .8f) transform.up = new Vector2(0, -1);
+        sprite.transform.up = Vector2.Lerp(new Vector2(sprite.transform.up.x, sprite.transform.up.y), groundNormal, rotSpd * Time.deltaTime);
+
+
+    }
+}
